dc1a355a6e1b2597006765df5e81c7f69bffa08f
malathipraka <malathipriya.p@gmail.com>
2022-10-31 12:46:20 +0530

    my task

---
+++ src/org/git2/Gitlasttask.java
@@ -0,0 +1,39 @@
+package org.git2;
+
+
+
+import org.testng.annotations.Test;
+
+public class Gitlasttask {
+
+	public class task1 {
+
+		@Test
+		private void test1() {
+			System.out.println("testmethod test1");
+		}
+
+		@Test
+		private void test2() {
+			System.out.println("testmethod test2");
+		}
+
+		
+		@Test
+		private void testb() {
+			System.out.println("testmethod testb");
+		}
+
+		@Test
+		private void testc() {
+			System.out.println("testmethod testc");
+		}
+
+		@Test
+		private void testa() {
+			System.out.println("testmethod testa");
+		}
+
+	}
+
+}
